94b35333191b80def3837e6e1041e56d5cb758d6
Vadim Faber <faber.vadim@gmail.com>
2020-01-26 14:14:26 +0300

    refactoring

---
+++ core/src/main/java/com/faberv/config/AppConfig.java
@@ -0,0 +1,29 @@
+package com.faberv.config;
+
+import com.faberv.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(GameConfig.class)
+@ComponentScan(basePackages = "com.faberv")
+public class AppConfig {
+
+    // === bean methods ===
+    @Bean
+    public NumberGenerator numberGenerator() {
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game() {
+        return new GameImpl();
+    }
+
+    @Bean
+    public MessageGenerator messageGenerator() {
+        return new MessageGeneratorImpl();
+    }
+}
